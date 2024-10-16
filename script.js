1282e538c55b4c5d6534804472fbe624ba8aabba
pvictorianizzaro713 <paulanizzaro85@gmail.com>
2024-10-16 15:43:30 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,31 @@
+//if significa "se"
+//else significa "senao"
+
+//if (condiçao) {codigo que sera executado}
+
+
+
+
+//variaveis
+//3 var, let, const
+//prompt pergunta algo ao usuario;
+//sempre que usamos dados textuais ""
+//string
+//let nome = prompt("qual é o seu nome?");
+
+//alert usamos para exibir uma mensagem
+//alert("seja bem vindo(a)," + nome);
+let idade = prompt("quantos anos voce tem?")
+
+
+//if (idade >= 18) {
+    alert("Voce ja pode tirar a carteira de motorista!!!")
+//}else{
+   // alert("Voce ainda nao tem idade para dirigir")
+//}
+
+let gostardepizza = prompt("voce gosta de pizza? (sim ou nao)");
+//===estritamente igual
+if (gostardepizza === "sim"){
+    alert("tudo bem, talvez voce goste de outra comida");
+}
